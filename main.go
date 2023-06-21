76ebbe5c25f0d073964d5356a53a21d5026fc508
eiei114 <nowaka37564@gmail.com>
2023-06-21 17:30:20 +0900

    update main.go

--- main.go
+++ main.go
@@ -8,49 +8,38 @@ import (
 )
 
 func main() {
-
-	// ここでタイムアウト
 	ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
 	defer cancel()
 
-	//conn, err := net.Dial("tcp", ":8080")
-	conn, err := DialWithContext(ctx, "tcp", ":8080")
+	conn, _, err := DialWithContext(ctx, "tcp", ":8080")
 	if err != nil {
 		panic(err)
 	}
-
-	_, err = conn.Write([]byte("Hello World!!"))
-	if err != nil {
-		panic(err)
-	}
-
-	//スコープから出るときに
 	defer conn.Close()
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		_, err = conn.Write([]byte("Hello World!!"))
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("書き込み")
+	}
+
 }
 
-func DialWithContext(ctx context.Context, network, address string) (net.Conn, error) {
-
-	fmt.Println("実行！")
+func DialWithContext(ctx context.Context, network, address string) (net.Conn, func() bool, error) {
 	conn, err := net.Dial(network, address)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	stopc := make(chan struct{})
 	stop := context.AfterFunc(ctx, func() {
 		fmt.Println("アフターファンク")
-		conn.SetDeadline(time.Now())
-		close(stopc)
+		conn.Close()
 	})
 
-	if !stop() {
-		// The AfterFunc was started.
-		// Wait for it to complete, and reset the Conn's deadline.
-		<-stopc
-		conn.SetReadDeadline(time.Time{})
-		fmt.Println("リセット")
-		return conn, ctx.Err()
-	}
-
-	return conn, nil
+	return conn, stop, nil
 }
